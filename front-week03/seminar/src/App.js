9848835afab7b7f49b2719e058904a3162998126
youbing5921 <ahdtlf2002@sookmyung.ac.kr>
2024-06-27 11:34:34 +0900

    front-week03

---
+++ front-week03/seminar/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import ExternalButton from "./ExternalButton";
+import FunnyButton from "./FunnyButton";
+import Hello from "./Hello";
+import InlineButton from "./InlineButton";
+import LikeButton from "./LikeButton";
+
+function App() {
+  return (
+    <>
+      <LikeButton />
+      <FunnyButton />
+      {/* <h1>멋사 프론트 3주차 세미나</h1>
+      <Hello />
+      <InlineButton />
+      <ExternalButton /> */}
+    </>
+  );
+}
+
+export default App;
