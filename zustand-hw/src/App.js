78e20e3e4033483d7c7171bb3d8e289fe12d6709
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-09-23 16:57:41 +0900

    [Feat] 메인 컴포넌트 세팅
    - 현재 장바구니에 담긴 상품들의 가격을 상단에 보여줌.
    - ItemBox를 세팅하고, 초기화 버튼을 구현하기 위해 useCartStore로 clearAmount 메소드를 불러옴.

--- zustand-hw/src/App.js
+++ zustand-hw/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import ItemBox from "./components/ItemBox";
+import useCartStore from "./store";
 
 function App() {
-  return <div></div>;
+  const amount = useCartStore((state) => state.amount);
+  const clearAmount = useCartStore((state) => state.clearAmount);
+
+  return (
+    <div>
+      <h4>현재 장바구니에 담긴 상품들의 가격은 {amount}원입니다.</h4>
+      <ItemBox />
+      <button onClick={clearAmount}>장바구니 초기화</button>
+    </div>
+  );
 }
 
 export default App;
