fd1c9ac42b75ee71e41503bb6cb0d465e4aba84e
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-09-30 14:41:25 +0900

    [Add] Router 설정

--- tanstack-query-hw/src/App.js
+++ tanstack-query-hw/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
+import MyPage from "./Pages/MyPage";
+import SignUp from "./Pages/SignUp";
+import UpdateProfile from "./Pages/UpdateProfile";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/myPage" />} />
+        <Route path="/myPage" element={<MyPage />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/updateProfile" element={<UpdateProfile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
