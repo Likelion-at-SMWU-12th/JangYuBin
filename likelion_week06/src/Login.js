931fe6b61a1fd9f4dd11d21f5ec3786ef422d5fd
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-10-21 13:05:14 +0900

    Step 1: 인가 코드 받기

--- likelion_week06/src/Login.js
+++ likelion_week06/src/Login.js
@@ -4,8 +4,12 @@ import google_btn_img from "./web_light_sq_SI@2x.png";
 
 const Login = () => {
   const authServerLink = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+
+  // 1. client가 resource server에 인가 코드 받기 요청
+  // https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-code
   const handleKakao = () => {
     window.location.href = authServerLink;
+    // 2. 인가 코드 받기 요청이 성공하면 Redirect URI로 연결되며 쿼리 코드로 인가 코드가 넘어옴.
   };
 
   return (
